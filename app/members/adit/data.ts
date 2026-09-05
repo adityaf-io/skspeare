4642396e2d2b618a82083cad47bfdfe8dce1b0cb
madityaf <149251596+adityaf-io@users.noreply.github.com>
2026-09-05 11:44:01 +0700

    Update member data for Adit with personal information

--- app/members/adit/data.ts
+++ app/members/adit/data.ts
@@ -5,20 +5,20 @@ const aditData: MemberData = {
   name: "Muhamad Aditya Firmasyah",
   role: "Anggota",
   // TODO: replace with your own NIM
-  nim: "",
+  nim: "M0403251113",
   photo: "/member/demo-user.jpg",
   // TODO: replace with your LinkedIn profile URL
-  linkedinUrl: "",
+  linkedinUrl: "https://www.linkedin.com/in/muhamad-aditya-firmansyah-02755137a/",
   // TODO: replace with your CV link or file path
   cvUrl: "",
   // TODO: replace with your hometown/region
-  hometown: "",
+  hometown: "Bekasi",
   // TODO: replace with your hobbies
   hobbies: [],
   // TODO: replace with your Instagram handle (without @)
   instagramHandle: "",
   // TODO: replace with your personal quote
-  quote: "",
+  quote: "makan bang",
   // TODO: replace with your Spotify track URL
   spotifyTrackUri: "",
 };
